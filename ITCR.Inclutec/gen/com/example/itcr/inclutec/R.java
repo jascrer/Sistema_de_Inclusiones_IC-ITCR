b48dc797a18bbe529974d85e881a38a88ed190b6
jascrer <jarnoldo2809@gmail.com>
2013-12-27 14:47:41 -0600

    Cambio de referencias en imagenes de Andriod

--- ITCR.Inclutec/gen/com/example/itcr/inclutec/R.java
+++ ITCR.Inclutec/gen/com/example/itcr/inclutec/R.java
@@ -34,12 +34,13 @@ or to a theme attribute in the form "<code>?[<i>package</i>:][<i>type</i>:]<i>na
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
         public static final int ic_logo_blanco=0x7f020001;
+        public static final int tecid=0x7f020002;
     }
     public static final class id {
         public static final int action_settings=0x7f090003;
-        public static final int imageView1=0x7f090002;
-        public static final int linearLayout1=0x7f090000;
-        public static final int progressBar1=0x7f090001;
+        public static final int imageView1=0x7f090000;
+        public static final int linearLayout1=0x7f090001;
+        public static final int progressBar1=0x7f090002;
     }
     public static final class layout {
         public static final int activity_login=0x7f030000;
