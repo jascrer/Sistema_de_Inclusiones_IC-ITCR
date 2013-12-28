c269fe83d7d00095ce75329002d2513d3a38f3e9
Juan José Rojas <juanrojas@ic-itcr.ac.cr>
2013-12-28 03:39:32 -0600

    Formulario de Inclusión
    Notas:
    - Sortable de los grupos y Accordions de los índex no están trabajando
    (al menos, a mi Chrome no me los quiere desplegar, pero supongo que es
    Capa 8)
    - Datos Alambrados por ahora en la info personal y en el autocomplete de
    los cursos.
    - Faltan estilos de centrado y formatos para los otros <h#>
    - A mi criterio faltan indicadores para saber si tiene RN o algún
    requisito sin cumplir...

--- ITCR.InclusionesTec/ITCR.InclusionesTec/Scripts/_sistema/jquery-widgets-applied.js
+++ ITCR.InclusionesTec/ITCR.InclusionesTec/Scripts/_sistema/jquery-widgets-applied.js
@@ -5,3 +5,20 @@
 $(function () {
     $(".accordion").accordion();
 });
+
+$(function () {
+    $("#sortable").sortable();
+    $("#sortable").disableSelection();
+});
+
+//(function ($) {
+
+//    var allPanels = $('.accordion > div').hide();
+
+//    $('.accordion > dt > a').click(function () {
+//        allPanels.slideUp();
+//        $(this).parent().next().slideDown();
+//        return false;
+//    });
+
+//})(jQuery);
